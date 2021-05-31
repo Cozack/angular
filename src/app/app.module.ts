86a9221a6b449a84aa89d62f9599b5385c690341
Cozack <dudycholeg@gmail.com>
2021-05-31 14:33:46 +0300

    angular with todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './component/comments/comments.component';
+import { CommentComponent } from './component/comment/comment.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TodoComponent } from './component/todo/todo/todo.component';
+import { TodosComponent } from './component/todos/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent,
+    CommentComponent,
+    TodoComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
